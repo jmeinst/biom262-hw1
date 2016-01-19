53317857f29edd7a926f2e268617580fc08c46d3
Jaclyn Einstein <jeinstei@tscc-login2.sdsc.edu>
2016-01-19 15:26:59 -0800

    Added #PBS and removed output lines

--- tf_binding.sh
+++ tf_binding.sh
@@ -1,39 +1,27 @@
-module load biotools
+#!/bin/bash
+#PBS -q hotel
+#PBS -N tf_binding.sh
+#PBS -l nodes=10:ppn=2
+#PBS -l walltime=:00:50:00
+#PBS -o tf_binding.sh.o
+#PBS -e tf_binding.sh.e
+#PBS -V
+#PBS -M <jeinstei@eng.ucsd.edu>
+#PBS -A jeinstei
+
+cd ~/code/biom262-2016/weeks/week01/data
 
 #Exercise 1
 # Filter the tf.bed file for only the NFKB\n
 awk '{if($4=="NFKB") print}' tf.bed > tf.nfkb.bed
-wc -l tf.nfkb.bed
-echo '--- First 10 lines ---'
-head tf.nfkb.bed
-echo '--- Random 10 lines ---'
-awk -v seed=907 'BEGIN{srand(seed);}{ if (rand() < 0.5 ) {print $0}}'  tf.nfkb.bed | head
-echo '--- Last 10 lines ---'
-tail tf.nfkb.bed
 
 #Exercise 2
 # Filter only the rows of the gtf file that contain the features of type "transcript"\n
 awk '{if($3=="transcript") print}' gencode.v19.annotation.chr22.gtf > gencode.v19.annotation.chr22.transcript.gtf
-wc -l gencode.v19.annotation.chr22.transcript.gtf
-echo '--- First 10 lines ---'
-head gencode.v19.annotation.chr22.transcript.gtf
-echo '--- Random 10 lines ---'
-awk -v seed=907 'BEGIN{srand(seed);}{ if (rand() < 0.5 ) {print $0}}'  gencode.v19.annotation.chr22.transcript.gtf | head
-echo '--- Last 10 lines ---'
-tail gencode.v19.annotation.chr22.transcript.gtf
 
 #Exercise 3
 # Use bedtools to find promoters (2000 bases upstream of gene)\n
 bedtools flank -i gencode.v19.annotation.chr22.transcript.gtf -g hg19.genome -l 2000 -r 0 -s > gencode.v19.annotation.chr22.transcript.promoter.gtf
-wc -l gencode.v19.annotation.chr22.transcript.promoter.gtf
-echo '--- First 10 lines ---'
-head gencode.v19.annotation.chr22.transcript.promoter.gtf
-echo '--- Random 10 lines ---'
-awk -v seed=907 'BEGIN{srand(seed);}{ if (rand() < 0.5 ) {print $0}}' gencode.v19.annotation.chr22.transcript.promoter.gtf | head
-echo '--- Last 10 lines ---'
-tail gencode.v19.annotation.chr22.transcript.promoter.gtf
-
-
-
 
 echo "Hello I am a message in standard out (stout)"
+
