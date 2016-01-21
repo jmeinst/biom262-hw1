238654c44a6e852852d5a5723b2c09deacfca2fc
Ben Rubin <Benemeryrubin@gmail.com>
2016-01-21 13:27:16 -0800

    just touching up the standard error output

--- tf_binding.sh
+++ tf_binding.sh
@@ -26,4 +26,4 @@ bedtools intersect -a gencode.v19.annotation.chr22.transcript.promoter.gtf -b tf
 bedtools getfasta -fi GRCh37.p13.chr22.fa -bed gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf -s -fo gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
 
 echo "Hello I am a message in standard out (stout)"
-echo "Hello I am a message in standard error (stderr)" >&2
+echo "Ben is to blame for this message in standard error (stderr)" >&2
