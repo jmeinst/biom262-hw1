34b095121d2700b608e00473a3828a33fd416961
Jaclyn Einstein <jeinstei@tscc-login2.sdsc.edu>
2016-01-20 18:01:55 -0800

    Edited PBS tags to get tscc to run. Also created data folder for files to be input and output into. Including tscc error and output files.

--- tf_binding.sh
+++ tf_binding.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 #PBS -q hotel
+#PBS -l nodes=2:ppn=2
+#PBS -l walltime=:00:20:00
 #PBS -N tf_binding.sh
-#PBS -l nodes=10:ppn=2
-#PBS -l walltime=:00:50:00
-#PBS -o tf_binding.sh.o
-#PBS -e tf_binding.sh.e
-#PBS -V
-#PBS -M <jeinstei@eng.ucsd.edu>
-#PBS -A jeinstei
 
-cd ~/code/biom262-2016/weeks/week01/data
+cd ~/code/biom262-hw1/data
+module load biotools
 
 #Exercise 1
 # Filter the tf.bed file for only the NFKB\n
@@ -23,12 +19,11 @@ awk '{if($3=="transcript") print}' gencode.v19.annotation.chr22.gtf > gencode.v1
 # Use bedtools to find promoters (2000 bases upstream of gene)\n
 bedtools flank -i gencode.v19.annotation.chr22.transcript.gtf -g hg19.genome -l 2000 -r 0 -s > gencode.v19.annotation.chr22.transcript.promoter.gtf
 
-echo "Hello I am a message in standard out (stout)"
-
 #Exercise 4
 bedtools intersect -a gencode.v19.annotation.chr22.transcript.promoter.gtf -b tf.nfkb.bed >gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
 
 #Exercise 5
 bedtools getfasta -fi GRCh37.p13.chr22.fa -bed gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf -s -fo gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
 
-echo "Hello I am a message in standard error (stderr) >&2
+echo "Hello I am a message in standard out (stout)"
+echo "Hello I am a message in standard error (stderr)" >&2
